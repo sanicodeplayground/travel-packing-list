0af9b742ca00bd72b1f7c50f1bcbc458d9bbef4d
sanicodeplayground <magiur99@gmail.com>
2024-09-21 14:05:38 +0100

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      id: Math.random(),
+      description,
+      quantity,
+      packed: false,
+    };
+    console.log(newItem);
+
+    onAddItems(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+
+  function handleQuantity(e) {
+    setQuantity(Number(e.target.value));
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select value={quantity} onChange={handleQuantity}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item ..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
